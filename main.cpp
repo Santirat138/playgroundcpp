d9b08c0d67baaf0f2f37353c91df0959d2852645
Santirat138 <115531724+Santirat138@users.noreply.github.com>
2023-09-13 08:23:49 +0000

    Create function isSameNum.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
+// Idea NULL 3: Set default num in class node = -1.
 #include<iostream>
 using namespace std;
+class node{
+	public:
+		int num=-1;
+		node *prev;
+		node *next;
+};
+node *null=new node();
 
+bool isSameNum(node **headRef, int numCheck);
 int main(){
+	node *head=NULL;
+	node **headR=&head;
+	node *tail=NULL;
+	node **tailR=&tail;
 	
+}
+bool isSameNum(node **headRef, int numCheck){
+	node *checkNode=*headRef;
+	bool status=false;
+	while(checkNode!=NULL){
+		if(checkNode->num==numCheck){
+			status=true;
+			break;
+		}
+		else{
+			checkNode=checkNode->next;
+		}
+	}
+	return status;
 }
