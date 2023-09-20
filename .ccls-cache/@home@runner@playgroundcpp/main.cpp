7663727160263174063c010307308691fe33d219
Santirat138 <115531724+Santirat138@users.noreply.github.com>
2023-09-20 08:44:33 +0000

    Warmup data structure

--- .ccls-cache/@home@runner@playgroundcpp/main.cpp
+++ .ccls-cache/@home@runner@playgroundcpp/main.cpp
@@ -1,82 +1,74 @@
 #include<iostream>
 using namespace std;
 class node{
-    public:
-        int num=-1;
-        node *next;
+	public:
+		int num=-1;
+		node *next;
 };
 node *nullNode=new node();
+bool isSameNum(node **headRef, int numIn);
 void addFirst(node **headRef, int newNum);
-void show(node **headRef);
 node *findNode(node **headRef, int target);
 node *findPrevNode(node **headRef, int target);
-void deleteNode(node **headRef, int target);
-/*-----------------------------------------*/ 
+void show()
 int main(){
-    node *head=NULL;
-    node **headR=&head;
-    for(int i=0;i<=5;i++){
-        addFirst(headR, i);
-    }
-	show(headR);
-	node *testPrev=findPrevNode(headR, 5);
-	cout<<endl<<testPrev->num;
+	node *head=NULL;
+	node **headR=&head;
+
+	for(int i=0;i<=5;i++){
+		addFirst(headR, i);
+	}
+	
 }
-/*-----------------------------------------*/ 
+bool isSameNum(node **headRef, int numIn){
+	bool status;
+	if(*headRef!=NULL){
+		node *checkNode;
+		checkNode=*headRef;
+		while(checkNode!=NULL){
+			if(checkNode->num==numIn){
+				status=true;
+				break;
+			}
+			else{
+				status=false;
+				checkNode=checkNode->next;
+			}
+		}
+	}
+	else if(*headRef==NULL){
+		status=false;
+	}
+	return status;
+}
 void addFirst(node **headRef, int newNum){
-    node *newNode=new node();
+	node *newNode=new node();
 	newNode->num=newNum;
-    if(*headRef==NULL){
-        *headRef=newNode;
-    }
-    else if(*headRef!=NULL){
-        newNode->next=*headRef;
-        *headRef=newNode;
-    }
-}
-void show(node **headRef){
-    node *showNode=*headRef;
-    while(showNode!=NULL){
-        cout<<showNode->num<<" ";
-        showNode=showNode->next;
-    }
-    cout<<endl<<endl;
+	if(headRef!=NULL){
+		newNode->next=*headRef;
+	}
+	*headRef=newNode;
 }
 node *findNode(node **headRef, int target){
-    node *currNode=*headRef;
-    while(currNode!=NULL){
-        if(currNode->num==target){
-            break;
-        }
-        else{
-            currNode=currNode->next;
-        }
-    }
-    return currNode;
+	if(*headRef!=NULL){
+		node *targetNode=*headRef;
+		while(targetNode!=NULL){
+			if(targetNode->num==target){
+				break;
+			}
+			else{
+				targetNode=targetNode->next;
+			}
+		}
+		return targetNode;
+	}
+	else{
+		return nullNode;
+	}
 }
 node *findPrevNode(node **headRef, int target){
-    node *prevNode=nullNode;
-    node *currNode=findNode(headRef, target);
-    if(currNode==*headRef){
-        return prevNode;
-    }
-    else if(currNode!=*headRef){
-        prevNode=*headRef;
-        while(currNode!=NULL){
-            if(currNode->num==target){
-                break;
-            }
-            else{
-                prevNode=currNode;
-                currNode=currNode->next;
-            }
-        }
-        return prevNode;
-    }
-    else{
-        return nullNode;
-    }
+	node *prevNode;
+	node *currNode=findNode(headRef, target);
+	
+	
 }
-/*void deleteNode(node **headRef, int target){
-    
-}*/
